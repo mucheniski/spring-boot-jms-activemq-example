b36e959402b21ee2945d313b027ca7589b009450
Diego Mucheniski <mucheniski@gmail.com>
2021-10-13 17:03:10 -0300

    created message listener

--- src/main/java/com/example/springbootjmsactivemqexample/SpringBootJmsActivemqExampleApplication.java
+++ src/main/java/com/example/springbootjmsactivemqexample/SpringBootJmsActivemqExampleApplication.java
@@ -1,7 +1,9 @@
 package com.example.springbootjmsactivemqexample;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.jms.core.JmsTemplate;
 
 @SpringBootApplication
 public class SpringBootJmsActivemqExampleApplication {
