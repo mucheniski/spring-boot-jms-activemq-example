97462d016249c646621d733bfaeb5073898931d4
Diego Mucheniski <mucheniski@gmail.com>
2021-10-13 16:26:32 -0300

    First commit

---
+++ src/main/java/com/example/springbootjmsactivemqexample/SpringBootJmsActivemqExampleApplication.java
@@ -0,0 +1,13 @@
+package com.example.springbootjmsactivemqexample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootJmsActivemqExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootJmsActivemqExampleApplication.class, args);
+	}
+
+}
